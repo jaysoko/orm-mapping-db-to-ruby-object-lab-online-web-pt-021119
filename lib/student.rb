edf119142aa739ec3a8260ee7c4e340846368285
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 18:22:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,10 @@ class Student
       WHERE name = ?
       LIMIT 1
       SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql,name).map do|row|
+      self.new_from_db(row).first
+    end
+  end
 
   end
 
