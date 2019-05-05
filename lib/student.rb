9f8558ff366887eff741081cae1cc698b0da66f4
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 18:22:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,8 @@ class Student
       LIMIT 1
       SQL
     DB[:conn].execute(sql,name).map do|row|
-      self.new_from_db(row).first
-    end
+      self.new_from_db(row)
+    end.first
   end
 
   def save
