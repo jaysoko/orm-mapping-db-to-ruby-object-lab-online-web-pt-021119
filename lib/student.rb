3bf1c1c79c3ae58b2636b5e1ee188b24a2719228
Jay Sokolowski <jaysoko@gmail.com>
2019-05-05 18:46:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,9 +61,11 @@ class Student
       WHERE grade = 10
       LIMIT ?
       SQL
-    DB[:conn].execute(sql).map do|row|
+    DB[:conn].execute(sql,limit).map do|row|
           self.new_from_db(row)
         end
+
+
   end
 
   def self.first_student_in_grade_10
